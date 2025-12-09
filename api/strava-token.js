10a55759f849cdbe8bfa8cf83b52356a5a952f8e
davcollo-debug <davcollo@gmail.com>
2025-12-09 11:51:37 +1100

    Change export to CommonJS module syntax

--- api/strava-token.js
+++ api/strava-token.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // Enable CORS
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -37,4 +36,4 @@ export default async function handler(req, res) {
     console.error('Token exchange error:', error);
     return res.status(500).json({ error: error.message });
   }
-}
+};
